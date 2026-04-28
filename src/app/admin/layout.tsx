0d9012eed12013b458a8469a2b44407e245a84b4
BarLouvain <barlouvain@gmail.com>
2026-04-28 21:31:07 +0200

    fix postcss config

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,7 @@
 import { AdminApp } from "@/components/admin/AdminApp";
 
+export const dynamic = "force-dynamic";
+
 export default function AdminRootLayout({ children }: { children: React.ReactNode }) {
   return <AdminApp>{children}</AdminApp>;
 }
