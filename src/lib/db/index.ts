704aecd1d9cd1206d197eeae2395c41228e34c99
BarLouvain <barlouvain@gmail.com>
2026-04-28 21:21:16 +0200

    css online

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -6,7 +6,7 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL, family: 4 });
+const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 
 export const db = drizzle(pool, { schema });
 
